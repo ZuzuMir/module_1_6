b39d30c69b20e76aca1f25b10aafffb5e48871e0
ZuzuMir <laraminnullina0247@gmail.com>
2024-10-10 23:41:11 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Aizat' : 2001 , 'Kamilla' : 2005, 'Alsou' : 2001 , 'Akin' : 1997}
+print(my_dict)
+print(my_dict['Kamilla'])
+print(my_dict.get('Airat'))
+my_dict.update({'Damir' : 1995 ,
+                'Ruslan' : 1992})
+print(my_dict.pop('Alsou'))
+print(my_dict)
+
+my_set = {2001 , 'hr' , 'finance' , 2005 , 2001, 'cook' , 'finance' , 2002 , 'hr'}
+print(my_set)
+my_set.add('artist')
+my_set.add(1999)
+my_set.remove('hr')
+print(my_set)
